bb1dc48f2e5e5f019a79da6081aee5efe93d7bb1
oh_junghyun <oh_junghyun@radcns.com>
2022-07-20 08:08:36 +0900

    전체 커밋

---
+++ JavaPractice/src/practice01/test.java
@@ -0,0 +1,16 @@
+package practice01;
+
+public class test {
+
+	public static void main(String[] args) {
+		if(true||false) {
+			//뒤에 있는거 검사 안함
+		}
+		if(true|false) {
+			//뒤에 있는거까지 검사
+		}
+		
+		
+	}
+
+}
